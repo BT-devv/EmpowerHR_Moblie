4849d1eb9b8696133e0674d17e0078c38a8c807d
oniqy <vyle.111003@gmail.com>
2024-12-28 12:46:03 +0700

    LoginAuthentication
    UI login , kết nối API login , xử lý các case sai mật khẩu , tài khoản không tồn tại

---
+++ lib/data/service/api_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiService {
+  static String createApiUrl(String endpoint) {
+    final port = dotenv.env['PORT'] ?? '3000'; // Lấy cổng từ .env
+    final baseUrl = 'http://192.168.1.9:$port/api/'; // URL
+    print('URL: $baseUrl$endpoint');
+    return '$baseUrl$endpoint';
+  }
+
+  Future<http.Response> postReq(Map<String, dynamic> data, String endpoint) async {
+    final url = createApiUrl(endpoint);
+    try {
+      print("Attempting to post data to $url with body: $data");
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(data),
+      );
+
+      return response;
+    } catch (error) {
+      print('Error occurred during POST request: $error');
+      rethrow;
+    }
+  }
+}
---
+++ lib/presentation/page/get_start.dart
@@ -0,0 +1,61 @@
+import 'package:empowerhr_moblie/presentation/page/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GetStartPage extends StatefulWidget {
+  const GetStartPage({super.key});
+
+  @override
+  State<GetStartPage> createState() => _GetStartPageState();
+}
+
+class _GetStartPageState extends State<GetStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFF4B5563),
+        body: Column(
+          children: [
+            Center(
+              child: Stack(
+                children: [
+                  Image.asset('assets/background.png'),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        LoginPage(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      return FadeTransition(
+                        opacity: animation,
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF466367),
+                  foregroundColor: Colors.white,
+                  fixedSize: const Size(274, 62),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15))),
+              child: Text(
+                'GET STARTED',
+                style: GoogleFonts.poppins(
+                    fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+            )
+          ],
+        ));
+  }
+}
